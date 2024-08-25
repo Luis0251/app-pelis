41529e3bd287f9e394dbef242c60e3fdf1600257
Lushito <luisdevcol@gmail.com>
2024-08-25 17:19:08 -0500

    [ADD]: added navbar and react dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/movies",
+    element: <h1> PAGINA DE MOVIES</h1>,
+  },
+  {
+    path: "/series",
+    element: <h1> PAGINA DE SERIES</h1>,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
